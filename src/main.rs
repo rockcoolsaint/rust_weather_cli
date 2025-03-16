ab614d51da4bccd6eb88c859006da573c8f8e21e
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-03-16 19:17:52 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use serde::Deserialize;
+
+// Struct to deserialize the JSON from openweatherAPI
+#[derive(Deserialize, Debug)]
+struct WeatherResponse {
+
+}
+fn main() {
+    println!("Hello, world!");
+}
